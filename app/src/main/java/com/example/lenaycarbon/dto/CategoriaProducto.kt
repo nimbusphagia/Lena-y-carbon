4a9e26ff9865900ee9a3d217022fdd9fe47fbacd
Ignacio Machuca <185011694+nimbusphagia@users.noreply.github.com>
2026-06-02 11:32:55 -0500

    add dto dir

---
+++ app/src/main/java/com/example/lenaycarbon/dto/CategoriaProducto.kt
@@ -0,0 +1,8 @@
+package com.example.lenaycarbon.dto
+
+enum class CategoriaProducto {
+    POLLO,
+    BEBIDA,
+    ACOMPANAMIENTO,
+    COMBO,
+}
---
+++ app/src/main/java/com/example/lenaycarbon/dto/Producto.kt
@@ -0,0 +1,10 @@
+package com.example.lenaycarbon.dto
+
+data class Producto(
+    val nombre:String,
+    val precio:Double,
+    val descripcion:String,
+    val imagen:Int,
+    val disponibilidad:Boolean,
+    val categoria: CategoriaProducto,
+)
